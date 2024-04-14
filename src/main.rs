f2f1147cffc320ee49cea11f3e4702a70fad0783
Subho Yadav <subho@gide.ai>
2024-04-14 15:43:59 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// Install rust on linux and macos: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+// starting a new rust project: cargo new your-project
+fn main () {
+    let name: &str = "Subho Yadav";
+    println!("Hi my name is: {}", name);
+}
---
+++ target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/subho/Desktop/project/rust/rust/target/debug/rust: /home/subho/Desktop/project/rust/rust/src/main.rs
