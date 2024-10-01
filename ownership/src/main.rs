05c48f6a5df6053f2c7e52b48cc30f2f8c7aed64
Subho Yadav <subho@gide.ai>
2024-10-01 20:09:10 +0530

    added notes

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -14,9 +14,49 @@ fn main() {
     s.push_str(" World");
     println!("{s}");
     
-    let s2 = s;
+    let _s2 = s;
 
-    println!("{}", s); // this line will generate an error
+    // When String that is allocated on the heap is created following is stored on the stack and is replicated when we try to bind it with another variable
+    // [
+    //     "pointer",
+    //     "capacity",
+    //     "length"
+    // ]
+
+    // println!("{}", s); // this line will generate an error
     //  The value of s is moved to s2 and s is no longer valid from this point
     // whenever a variable goes out of scope Rust calls a special function called "drop" similar to RAII(Resouce Acquisition Is Initialisation) pattern in C++
+
+    let mut my_name: String = String::from("Subho Yadav");
+    my_name = takes_ownership(my_name); // the ownership of my_name is transfered to the function takes_ownership
+
+    println!("My name is: {}", my_name);
+
+    let age: i32 = 24;
+    makes_copy(age);
+
+
+    // Returning a tuple from the function 
+    let (my_name_after_ownership_returned, length) = calculates_length(my_name);
+
+    println!("{} has a length of: {}", my_name_after_ownership_returned, length);
+    
 }
+
+fn calculates_length(word: String) -> (String, usize) {
+    let length = word.len();
+
+    (word, length)
+}
+
+fn takes_ownership(some_string: String) -> String {
+    println!("{}", some_string);
+    // This is very inconvinient to return whatever we recieve when we want to reuse the parameter in the parent function and to overcome this we have "references"
+    some_string
+}
+
+fn makes_copy(some_value: i32) {
+    println!("{} ", some_value);
+}
+
+// Any type that implements the 'copy' trait is copied and is not moved
